95fcb3e4a45999c03da6505e095fa0000ee46fe3
LUIZ HENRIQUE PAROLIM DOMINGUES <luizhpdomingues2@gmail.com>
2023-03-23 10:48:38 -0300

    Primeiro commit

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// importamos as bibliotecas necessárias
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes_1 = require("./routes");
+// criamos uma instancia de aplicativo express
+const app = express();
+// necessários para suportar tipo json
+app.use(bodyParser.json());
+// necessário para url
+app.use(bodyParser.urlencoded({ extended: false }));
+// rotas
+app.use('/routes', routes_1.Routes);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`API aberta em http://localhost:${port}/`);
+});
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+
+// importamos as bibliotecas necessárias
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { Routes } from './routes';
+
+// criamos uma instancia de aplicativo express
+const app: express.Application = express();
+
+// necessários para suportar tipo json
+app.use(bodyParser.json());
+// necessário para url
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// rotas
+app.use('/routes', Routes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`API aberta em http://localhost:${port}/`);
+});

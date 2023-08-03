6e174cbbef195aee239db66e4509ef74570af546
Luiz Domingues <luizhpdomingues2@gmail.com>
2023-08-03 11:38:31 -0300

    03.08

--- dist/index.js
+++ dist/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 // necessário para url
 app.use(bodyParser.urlencoded({ extended: false }));
 // rotas
-app.use('/routes', routes_1.Routes);
+app.use('/api', routes_1.Routes);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`API aberta em http://localhost:${port}/`);
--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // rotas
-app.use('/routes', Routes);
+app.use('/api', Routes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
